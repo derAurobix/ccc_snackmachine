4dda436b324af0340393650054f027abf5950008
derAurobix <161wit02@o365.htl-leoben.at>
2018-04-11 13:56:05 +0200

    Testdaten einlesen

--- level_1.java
+++ level_1.java
@@ -4,7 +4,16 @@ public class level_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		int Betrag = Integer.parseInt(args[0]);
+		int anzahlMuenzen = Integer.parseInt(args[1]);
+		int[] muenzen = new int[anzahlMuenzen];
+		
+		for(int m = 0; m < anzahlMuenzen;m ++) {
+			int offset = 2;
+			muenzen[m] = Integer.parseInt(args[offset + m]);			
+		}
+		
 	}
 
 }
